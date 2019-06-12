234829bbe8ff6cadcb66991380cb0b38aa749331
Valena Del Valle <valenadelvalle4@gmail.com>
2019-06-12 18:28:40 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,4 +1,4 @@
-def oxford_comma(array)
+def xoxford_comma(array)
   if array.length == 2 
     "#{array[0]} and #{array[1]}"
     
@@ -10,7 +10,8 @@ end
 def oxford_comma(array)
   if array.length <= 7
     "#{array[0]}, #{array[1]}, #{array[2]}, #{array[3]}, #{array[4]}, #{array[5]}, and #{array[6]}"
-
+  end
+end 
 
 
 
