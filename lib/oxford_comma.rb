e00a376ccbff569d4c490f2f635f002fb378c12a
Valena Del Valle <valenadelvalle4@gmail.com>
2019-06-12 19:02:41 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,18 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    "#{array[0]}"
+  elsif array.length == 2 
+    "#{array[0]} and #{array[1]}"
+  else  
+    last_item = array.pop
+    array.join(", ") << ", and " << last_item
+  end
+# ["kiwi"].join
+#  ["kiwi", "durian"].join(" and ")
 end
+
+
+
+
+
+
