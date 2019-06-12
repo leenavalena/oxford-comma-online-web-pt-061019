17fc2b921741e6f9bb9f1d66b7887c2291887f7e
Valena Del Valle <valenadelvalle4@gmail.com>
2019-06-12 18:35:12 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,7 +3,7 @@ def oxford_comma(array)
     "#{array[0]}"
   elsif array.length == 2 
     "#{array[0]} and #{array[1]}"
-    
+  else   
   end
 # ["kiwi"].join
 #  ["kiwi", "durian"].join(" and ")
