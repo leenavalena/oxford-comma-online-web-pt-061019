363afc76dd502b4417e0a43b1c434eecfecd3b8f
Valena Del Valle <valenadelvalle4@gmail.com>
2019-06-12 18:29:54 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,4 +1,4 @@
-def xoxford_comma(array)
+def oxford_comma(array)
   if array.length == 2 
     "#{array[0]} and #{array[1]}"
     
@@ -7,11 +7,7 @@ def xoxford_comma(array)
 #  ["kiwi", "durian"].join(" and ")
 end
 
-def oxford_comma(array)
-  if array.length <= 7
-    "#{array[0]}, #{array[1]}, #{array[2]}, #{array[3]}, #{array[4]}, #{array[5]}, and #{array[6]}"
-  end
-end 
+
 
 
 
