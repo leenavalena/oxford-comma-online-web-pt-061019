ae9b1ed4d7111e4faec0afd6c6c97fa54085d97f
Valena Del Valle <valenadelvalle4@gmail.com>
2019-06-12 18:50:28 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,7 +4,7 @@ def oxford_comma(array)
   elsif array.length == 2 
     "#{array[0]} and #{array[1]}"
   else  
-    last_item = array.last
+    last_item = array.pop
     array.join(", ") << ", and"
   end
 # ["kiwi"].join
